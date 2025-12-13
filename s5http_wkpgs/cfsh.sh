77cf06f342c50e35b25636440e1343fd4b8a0f3e
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2025-12-13 20:28:29 +0800

    Update cfsh.sh

--- s5http_wkpgs/cfsh.sh
+++ s5http_wkpgs/cfsh.sh
@@ -69,7 +69,7 @@ chmod +x "$HOME/cfs5http/cf_$port.sh"
 bash "$HOME/cfs5http/cf_$port.sh"
 echo "安装完毕，Socks5/Http节点已在运行中，查看运行日志请选择3" && sleep 5
 echo
-until grep -q '服务端域名与端口\|客户端地址与端口\|运行中的优选IP' "$HOME/cfs5http/$port.log"; do sleep 1; done; grep -m1 '服务端域名与端口\|客户端地址与端口\|运行中的优选IP' "$HOME/cfs5http/$port.log"
+until grep -q '服务端域名与端口' "$HOME/cfs5http/$port.log" && grep -q '客户端地址与端口' "$HOME/cfs5http/$port.log" && grep -q '运行中的优选IP' "$HOME/cfs5http/$port.log"; do sleep 1; done; grep -m1 '服务端域名与端口\|客户端地址与端口\|运行中的优选IP' "$HOME/cfs5http/$port.log"
 echo
 elif [ "$menu" = "3" ]; then
 showmenu
