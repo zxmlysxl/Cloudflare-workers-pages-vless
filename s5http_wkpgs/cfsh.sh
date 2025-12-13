9477d2fe5757e0496a808eb7811c957fcd38e6e4
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2025-12-13 22:07:20 +0800

    Update cfsh.sh

--- s5http_wkpgs/cfsh.sh
+++ s5http_wkpgs/cfsh.sh
@@ -75,7 +75,7 @@ nohup $HOME/cfs5http/cfwp client_ip=:"$port" dns="$dns" cf_domain="$cf_domain" c
 EOF
 chmod +x "$HOME/cfs5http/cf_$port.sh"
 bash "$HOME/cfs5http/cf_$port.sh"
-echo "安装完毕，Socks5/Http节点已在运行中，查看运行日志请选择3" && sleep 5
+echo "安装完毕，Socks5/Http节点已在运行中，可进入菜单选择2，查看节点配置信息及日志" && sleep 5
 echo
 until grep -q '服务端域名与端口\|客户端地址与端口\|运行中的优选IP' "$HOME/cfs5http/$port.log"; do sleep 1; done; head -n 16 "$HOME/cfs5http/$port.log" | grep '服务端域名与端口\|客户端地址与端口\|运行中的优选IP'
 echo
